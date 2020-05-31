7a9b277e6377eab562d90d3bf1194beb09c424ab
hgncxzy <hgncxzy@qq.com>
2020-05-31 20:12:24 +0800

    remove unused import.

--- broadcastbestpractice/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ broadcastbestpractice/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.broadcastbestpractice;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,7 +12,6 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 
 import com.example.broadcastbestpractice.base.BaseActivity;
-import com.example.broadcastbestpractice.controller.ActivityController;
 import com.example.broadcastbestpractice.permission.FloatTool;
 
 /**
